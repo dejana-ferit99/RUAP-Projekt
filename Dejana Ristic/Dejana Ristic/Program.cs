e975b060ceac0f6b057934784a54a1b79cba6559
cindricmarko <bruno.junakovic@gmail.com>
2022-10-25 17:39:05 +0200

    projekt ruap lv1

---
+++ Dejana Ristic/Dejana Ristic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dejana_Ristic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Dejana Ristic");
+        }
+    }
+}
